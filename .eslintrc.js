7cdb6f907610fee82541394330461c14c18b09ea
854 <jahae854@gmail.com>
2023-10-26 17:33:59 +0900

    change to esm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
-module.exports = {
+const eslintrc = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 };
+
+export default eslintrc;
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  roots: ["<rootDir>/src"],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
+export const roots = ["<rootDir>/src"];
+export const testMatch = [
+  "**/__tests__/**/*.+(ts|tsx|js)",
+  "**/?(*.)+(spec|test).+(ts|tsx|js)",
+];
+export const transform = {
+  "^.+\\.(ts|tsx)$": "ts-jest",
 };
