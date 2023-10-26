5cc00bc2873963b478b7f426bb395520af196cb3
854 <jahae854@gmail.com>
2023-10-26 17:30:53 +0900

    PJT INIT

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+export const delayMillis = (delayMs: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, delayMs));
+
+export const greet = (name: string): string => `Hello ${name}`;
+
+/** main test */
+export const foo = async (): Promise<boolean> => {
+  console.log(greet("World"));
+  await delayMillis(1000);
+  console.log("done");
+  return true;
+};
