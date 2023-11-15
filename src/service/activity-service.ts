125204795d64f017c5393278d0ded9dfa4ff990e
854 <jahae854@gmail.com>
2023-11-16 02:00:07 +0900

    add service

---
+++ src/service/activity-service.ts
@@ -0,0 +1,36 @@
+/**
+ * Activity service
+ * 액티비티 객체의 생성, 수정, 삭제, 조회를 담당하는 서비스
+ */
+
+const activityService = {
+  /**
+   * Create Activity
+   */
+  create: () => {
+    return;
+  },
+
+  /**
+   * Read Activity
+   */
+  read: () => {
+    return;
+  },
+
+  /**
+   * Update Activity
+   */
+  update: () => {
+    return;
+  },
+
+  /**
+   * Delete Activity
+   */
+  delete: () => {
+    return;
+  },
+};
+
+export default activityService;
---
+++ src/service/file-service.ts
@@ -0,0 +1,16 @@
+/**
+ * File service
+ * 파일 업로드 및 다운로드를 담당하는 서비스
+ */
+
+const fileService = {
+  upload: () => {
+    return;
+  },
+
+  download: () => {
+    return;
+  },
+};
+
+export default fileService;
