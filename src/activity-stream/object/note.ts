752b2da21fc7d3f42918308683ab4237c2f3e153
854 <jahae854@gmail.com>
2023-11-16 00:54:42 +0900

    add Note class

--- src/activity-stream/object/note.ts
+++ src/activity-stream/object/note.ts
@@ -7,6 +7,7 @@ import { WithPublishDate } from "../types/util";
  * https://www.w3.org/TR/activitystreams-vocabulary/#dfn-note
  */
 export interface AsNote extends WithPublishDate<ASObject> {
+  id: string;
   type: "Note";
   name: string;
   content: string; // HTML content
@@ -15,3 +16,36 @@ export interface AsNote extends WithPublishDate<ASObject> {
   replies: ASObject[];
   isreplyto: ASObject;
 }
+
+class Note implements AsNote {
+  id: string;
+  type: "Note" = "Note";
+  name: string;
+  content: string;
+  image: string | AsImage | null;
+  actor: ASObject;
+  replies: ASObject[];
+  isreplyto: ASObject;
+  createdAt: string;
+  updatedAt?: string;
+  deletedAt?: string | undefined;
+
+  toJSON(): this {
+    return this;
+  }
+
+  constructor(params: AsNote) {
+    this.id = params.id;
+    this.name = params.name;
+    this.content = params.content;
+    this.image = params.image;
+    this.actor = params.actor;
+    this.replies = params.replies;
+    this.isreplyto = params.isreplyto;
+    this.createdAt = params.createdAt;
+    this.updatedAt = params.updatedAt;
+    this.deletedAt = params.deletedAt;
+  }
+}
+
+export default Note;
