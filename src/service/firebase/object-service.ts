be098fb5d81b0a385a0d703e15ea820755d2ae12
854 <jahae854@gmail.com>
2023-11-19 02:20:01 +0900

    add read

--- src/service/firebase/object-service.ts
+++ src/service/firebase/object-service.ts
@@ -2,7 +2,20 @@
  * Object service
  * 오브젝트 객체의 생성, 수정, 삭제, 조회를 담당하는 서비스
  */
-import { Timestamp, deleteDoc, doc, setDoc } from "firebase/firestore";
+import {
+  Timestamp,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  setDoc,
+  startAfter,
+  where,
+} from "firebase/firestore";
 import { AsObject } from "../../activity-stream/types/core";
 import { WithPublishDate } from "../../activity-stream/types/util";
 import { firebaseDB } from "./config/init";
@@ -28,8 +41,62 @@ const objectService = {
 
   /**
    * Read object
+   * TODO : 피드 화면 진입시, 페이징 진행시, 마이페이지 진입시 쿼리 조건을 분기하여 쿼리를 날린다.
    */
-  read: () => {
+  read: async (option: {
+    ids?: string[];
+    lastItem?: string;
+    actor?: string;
+    limitCount?: number;
+    type?: string;
+  }) => {
+    const { lastItem, actor, limitCount = 10, type } = option || {};
+
+    // Collection 을 가져온다.
+    const objCollection = collection(firebaseDB, COLLECTION_NAME.OBJECT);
+
+    const lastItemRef = doc(
+      firebaseDB,
+      `${COLLECTION_NAME.OBJECT}/${lastItem}`
+    );
+
+    const lastItemSnapshot = lastItem ? await getDoc(lastItemRef) : null;
+
+    // 피드 화면 진입시 쿼리조건
+    const firstQuery = query(
+      objCollection,
+      orderBy("createdAt", "desc"),
+      limit(limitCount)
+    );
+
+    // 페이징 진행시 쿼리조건
+    const startAfterQuery = query(
+      objCollection,
+      orderBy("createdAt", "desc"),
+      limit(limitCount),
+      startAfter(lastItemSnapshot)
+    );
+
+    // 마이페이지 진입시 쿼리 조건
+    const actorQuery = actor
+      ? query(
+          objCollection,
+          orderBy("createdAt", "desc"),
+          where("author", "==", actor),
+          limit(limitCount)
+        )
+      : null;
+
+    // 비동기 요청 날리기
+    const finalQuery = (() => {
+      if (actor) {
+        return actorQuery;
+      }
+      return lastItem ? startAfterQuery : firstQuery;
+    })();
+
+    const postRes = await getDocs(finalQuery);
+
     return;
   },
 
