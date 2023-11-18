86a39334dff938e1299293b32667e0fcd64999e1
854 <jahae854@gmail.com>
2023-11-18 21:43:18 +0900

    add COLLECTION_NAME

---
+++ src/service/firebase/config/collection-name.ts
@@ -0,0 +1,11 @@
+/**
+ * Collection name
+ */
+const COLLECTION_NAME = {
+  OBJECT: "object",
+  ACTIVITY: "activity",
+  PROFILE: "profile",
+  PLACE: "place",
+};
+
+export default COLLECTION_NAME;
